2a0e316fdc86da50f09e51581c762b013b694589
iheanyiekwe <contactiheanyi@gmail.com>
2022-08-10 19:13:27 -0500

    All images

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ void main() {
         ),
         body: Center(
           child: Image(
-            image: AssetImage('images/pooricon.png'),
+            image: AssetImage('images/'),
           ),
         ),
       ),
